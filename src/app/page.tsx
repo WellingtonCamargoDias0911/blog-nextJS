75c290c5ccd0701ceaaea257f962394bee37f3d8
Wellington Dias <wellington0911@gmail.com>
2026-08-02 14:55:35 -0300

    Componente FeaturePost + Tailwind CSS Grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,34 @@
+import { Container } from '@/components/Container';
+import { Header } from '@/components/Header';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
 import clsx from 'clsx';
+import Image from 'next/image';
+import Link from 'next/link';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
-    <div>
-      <header>
-        <h1 className={clsx('text-6xl', 'font-bold', 'text-center', 'py-8')}>
-          Aqui é a Header!
-        </h1>
-      </header>
+    <Container>
+      <Header />
+
+      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
+        <Link className='w-full h-full overflow-hidden rounded-2xl' href='#'>
+          <Image
+            className='group-hover:scale-105 transition'
+            src='/images/bryen_0.png'
+            width={1200}
+            height={720}
+            alt={'Título do post'}
+          />
+        </Link>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+          consequuntur possimus tenetur iure consequatur sint fugiat vero,
+          dolorum alias assumenda laboriosam veniam nesciunt temporibus
+          repudiandae a enim, velit libero! Iste.
+        </div>
+      </section>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
@@ -21,6 +39,6 @@ export default async function HomePage() {
           Footer
         </p>
       </footer>
-    </div>
+    </Container>
   );
 }
