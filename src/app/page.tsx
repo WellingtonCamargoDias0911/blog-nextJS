8116279f62b00e4aa230a7449ea75e6594638d29
Wellington Dias <wellington0911@gmail.com>
2026-08-02 16:21:26 -0300

    Componente PostImageCover com Link + Image do next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import { Container } from '@/components/Container';
 import { Header } from '@/components/Header';
+import { PostCoverImage } from '@/components/PostCoverImage';
 import { PostHeading } from '@/components/PostHeading';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
 import clsx from 'clsx';
-import Image from 'next/image';
-import Link from 'next/link';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
@@ -14,16 +13,18 @@ export default async function HomePage() {
       <Header />
 
       <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
-        <Link className='w-full h-full overflow-hidden rounded-2xl' href='#'>
-          <Image
-            className='w-full h-full object-cover object-center group-hover:scale-105 transition'
-            src='/images/bryen_0.png'
-            width={1200}
-            height={720}
-            alt={'Título do post'}
-            priority
-          />
-        </Link>
+        <PostCoverImage
+          linkProps={{
+            href: 'post/asdasd',
+          }}
+          imageProps={{
+            width: 1200,
+            height: 720,
+            src: '/images/bryen_9.png',
+            alt: 'Alt da imagem',
+            priority: true,
+          }}
+        />
         <div className='flex flex-col gap-4 sm:justify-center'>
           <time
             className='text-slate-600 block text-sm/tight'
